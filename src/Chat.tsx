327bd014823b055b3f1b3d310f0be3eb51c3dde6
Alejo <alejo.bonadeo@educabot.com>
2025-12-15 16:55:16 -0300

    Fix messages

--- src/Chat.tsx
+++ src/Chat.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import  { useEffect, useRef, useState } from "react";
 
 const ChatWidget = () => {
   const [isOpen, setIsOpen] = useState(false);
